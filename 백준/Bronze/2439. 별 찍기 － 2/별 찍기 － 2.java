fe3242ad3d02df439ec144f295da6837e5b5e406
PgTomin <100673146+PgTomin@users.noreply.github.com>
2022-08-21 12:57:48 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 360 ms, Memory: 18392 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		try (Scanner sc = new Scanner(System.in)) {
+			int num = sc.nextInt();
+			sc.close();
+			
+			for(int i = 1; i <= num; i++) {
+				for(int j = num - i; j >= 1; j--) {
+					System.out.print(" ");
+				}
+				for(int x = 1; x <= i; x++) {
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+		}
+
+	}
+
+}
